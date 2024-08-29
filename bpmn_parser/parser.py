d12525d3eb791c41efadd8655786ac2ac3a5e060
Daniel Bailo <danbailoufms@gmail.com>
2024-08-28 22:45:26 -0300

    docs: trying to push on main

--- bpmn_parser/parser.py
+++ bpmn_parser/parser.py
@@ -10,6 +10,7 @@ from bpmn_parser.user_task import UserTask
 
 
 class BPMNParser:
+    """"""
     def __init__(self, path: Path):
         xml_parser = XMLParser(remove_blank_text=True)
         self.tree: _ElementTree = parse(path, xml_parser)
